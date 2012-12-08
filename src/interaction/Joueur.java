5953ba84ecd52b061f13ee1dd4fe65c877016855
Ludovic <ludoviclrds@gmail.com>
2012-12-09 00:44:48 +0100

    creation methode pour reorganiser le tableau
    Ne fonctionn pas encore erreur :
    java.lang.ArrayIndexOutOfBoundsException

--- src/interaction/Joueur.java
+++ src/interaction/Joueur.java
@@ -9,12 +9,12 @@ public class Joueur {
 
 	private int nbCredit;
 	private String nomJoueur;
-	
-		
-    public Student studentEnDeclin;
-    public Student studentActif;
-    
-    /*--Creation des Students----*/
+
+
+	public Student studentEnDeclin;
+	public Student studentActif;
+
+	/*--Creation des Students----*/
 	private Student unTC = Student.TC;
 	private Student unMaster  = Student.Master;
 	private Student unLicense = Student.License;
@@ -23,88 +23,103 @@ public class Joueur {
 	/*-------tableau d'étudiants disponibles----*/
 	private Student[] tabStudent= new Student[3];
 
+public void reorganiserTableau(Object tab[],int index)
+{
+	Object tempo;
+	tempo=tab[tab.length];
+	tab[index]=tempo;
+	System.out.println("ca passe avant la boucle");
 	
-	
-    public Joueur()
-    {
-    	tabStudent[0]=unTC;
-    	tabStudent[1]=unMaster;
-    	tabStudent[2]=unLicense;
-    }
-    
-	 public void setNomJoueur (String nom)
-	 {
-		 this.nomJoueur=nom;
-		 
-	 }
-	 
-	 public String getNomJoueur()
-	 {
-		 return nomJoueur;
-	 }
-	 
-	 public void passerEtudiantEnDeclin() 
-	 {
-		  
-	 }
-	
-	  public void deplacerHeures() 
-	  {
-		  
-	  }
-	
-	  public void lancerDe() 
-	  {
-		  
-	  }
-	
-	  public void finirTour() 
-	  {
-		  
-	  }
-	
-	  public void choisirEtudiant(Student stu) 
-	  {
-		  studentActif=stu;  
-	  }
-	
-	  public void selectionEtudiant()
-		{
-			System.out.println("Joueur 1 vous avez le choix entre : ");			
-
-
-			/*----Affichage des etudiants--------------*/
-			for (int i=0; i<tabStudent.length; i++)
-				System.out.println(i+1 +" : "+tabStudent[i].getDescription());
-			/*----------------------------------------*/
-
-			/*---------Cin de l'utilisateur ------*/
-			Scanner in = new Scanner(System.in);
-			int choix = in.nextInt();
-			/*---------Fin Cin de l'utilisateur ------*/
-
-			switch (choix)
-			{ 
-			case 1: this.choisirEtudiant(unTC);
-			System.out.println("Felicitation, vous êtes un "+tabStudent[0]);
-			break;
-
-			case 2:this.choisirEtudiant(unMaster);
-			System.out.println("Felicitation, vous êtes un "+tabStudent[1]);
-			break;
-
-			case 3:this.choisirEtudiant(unLicense);
-			System.out.println("Felicitation, vous êtes un "+tabStudent[2]);
-			break;
-
-			default: 
-				System.out.println("Le choix plante ...");
-
-			}
-			
-			//tabStudent[choix]=null;
-			Arrays.sort(tabStudent, choix, 2);
+	for(int i=index; i<tab.length-index-1; i++)
+	{	
+		System.out.println("ca passe dans la boucle");
+		tab[tab.length-i]=tab[index+i];
 		
+		//tab[index+i+1]=tab[index+i];
+	}
+	
+}
+
+	public Joueur()
+	{
+		tabStudent[0]=unTC;
+		tabStudent[1]=unMaster;
+		tabStudent[2]=unLicense;
+	}
+
+	public void setNomJoueur (String nom)
+	{
+		this.nomJoueur=nom;
+
+	}
+
+	public String getNomJoueur()
+	{
+		return nomJoueur;
+	}
+
+	public void passerEtudiantEnDeclin() 
+	{
+
+	}
+
+	public void deplacerHeures() 
+	{
+
+	}
+
+	public void lancerDe() 
+	{
+
+	}
+
+	public void finirTour() 
+	{
+
+	}
+
+	public void choisirEtudiant(Student stu) 
+	{
+		studentActif=stu;  
+	}
+
+	public void selectionEtudiant()
+	{
+		System.out.println("Joueur 1 vous avez le choix entre : ");			
+
+
+		/*----Affichage des etudiants--------------*/
+		for (int i=0; i<tabStudent.length; i++)
+			System.out.println(i+1 +" : "+tabStudent[i].getDescription());
+		/*----------------------------------------*/
+
+		/*---------Cin de l'utilisateur ------*/
+		Scanner in = new Scanner(System.in);
+		int choix = in.nextInt();
+		/*---------Fin Cin de l'utilisateur ------*/
+
+		switch (choix)
+		{ 
+		case 1: this.choisirEtudiant(unTC);
+		System.out.println("Felicitation, vous êtes un "+tabStudent[0].getDescription());
+		break;
+
+		case 2:this.choisirEtudiant(unMaster);
+		System.out.println("Felicitation, vous êtes un "+tabStudent[1].getDescription());
+		break;
+
+		case 3:this.choisirEtudiant(unLicense);
+		System.out.println("Felicitation, vous êtes un "+tabStudent[2].getDescription());
+		break;
+
+		default: 
+			System.out.println("Le choix plante ...");
+
 		}
-	  
+
+		
+		this.reorganiserTableau(tabStudent, choix);
+
+	}
+
 }
